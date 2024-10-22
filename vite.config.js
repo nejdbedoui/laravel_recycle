716b19dfe6956c0244b6bfc1a87ffd16699f9325
nejdbedoui <74482153+nejdbedoui@users.noreply.github.com>
2024-10-22 17:54:08 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        strictPort: true
+    },
     plugins: [
         laravel({
             input: [
